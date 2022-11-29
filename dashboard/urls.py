bfcc3c92aa876943d7800de189f524f6bf9eae77
ClarkLe01 <clark.le.goldenowl@gmail.com>
2022-11-29 17:21:52 +0700

    add candidate list

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, CandidateView
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    path('candidatelist/', CandidateView.as_view(), name='candidatelist'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views import View
 
 
 # Create your views here.
 @login_required(login_url='login')
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
+
+
+class CandidateView(View):
+    def get(self, request):
+        return render(request, 'dashboard/candidate/candidate_list.html')
