086612004649d3e3fa900d59eef5550ad49c53a9
ClarkLe01 <clark.le.goldenowl@gmail.com>
2022-11-29 16:55:53 +0700

    update _user_status.html <delete unused features>

--- job/urls.py
+++ job/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import JobListView
+from .views import JobListView, AddJobView
 
 urlpatterns = [
     path('joblist/', JobListView.as_view(), name='joblist'),
+    path('addjob/', AddJobView.as_view(), name='addjob'),
 ]
--- job/views.py
+++ job/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, CreateView
@@ -5,7 +6,7 @@ from .models import Job
 
 
 # Create your views here.
-class JobListView(View):
+class JobListView(LoginRequiredMixin, View):
     def get(self, request):
         jobs = Job.objects.filter(user=request.user)
         context = {
@@ -14,4 +15,10 @@ class JobListView(View):
         return render(request, 'dashboard/job/job_list.html', context)
 
 
+class AddJobView(LoginRequiredMixin, View):
+    def get(self, request):
+        context = {}
+        return render(request, 'dashboard/job/add_job.html', context)
 
+    def post(self, request):
+        pass
